45da584f25539a74b80e2da09c6f7b2646825359
jcitog <jcarayav625@gmail.com>
2019-09-13 19:51:42 -0300

    add sorcerer test

---
+++ test/model/units/SorcererTest.java
@@ -0,0 +1,44 @@
+package model.units;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import model.items.IEquipableItem;
+import model.items.MagicBookOscuridad;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ignacio Slater Muñoz
+ */
+public class SorcererTest extends AbstractTestUnit {
+
+  private Sorcerer sorcerer;
+  private MagicBookOscuridad magicBookOscuridad;
+
+  /**
+   * Set up the main unit that's going to be tested in the test set
+   */
+  @Override
+  public void setTestUnit() {
+    sorcerer = new Sorcerer(50, 2, field.getCell(0, 0));
+  }
+
+  /**
+   * @return the current unit being tested
+   */
+  @Override
+  public IUnit getTestUnit() {
+    return sorcerer;
+  }
+
+  /**
+   * Checks if the magic book is equipped correctly to the unit
+   */
+  @Test
+  @Override
+  public void equipAxeTest() {
+    assertNull(sorcerer.getEquippedItem());
+    sorcerer.equipItem(magicBookOscuridad);
+    assertEquals(magicBookOscuridad, sorcerer.getEquippedItem());
+  }
+}
