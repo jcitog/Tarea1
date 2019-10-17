cc9f4627b886a32b0671e9a45348d5ec824dd515
jcitog <jcarayav625@gmail.com>
2019-10-17 17:13:46 -0300

    add new files tarea2

---
+++ src/model/tacticians/Tactician.java
@@ -0,0 +1,19 @@
+package model.tacticians;
+
+/**
+ * @author Ignacio Slater Muñoz
+ * @since
+ */
+public class Tactician {
+
+  /**
+   * Creates a new Tactician.
+   * <p>
+   * Tacticians are
+   * one.
+   *
+   * @param
+   */
+
+
+}
